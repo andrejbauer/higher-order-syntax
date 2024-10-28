4007ddd3f909441e2468234643bdbb679181db6b
Andrej Bauer <Andrej.Bauer@andrej.com>
2024-10-28 16:05:49 +0100

    Properties of renamings

--- HigherRankedSyntax/HigherRankedSyntax/Renaming.lean
+++ HigherRankedSyntax/HigherRankedSyntax/Renaming.lean
@@ -7,18 +7,15 @@ infix:25 " →ʳ " => Renaming
 
 namespace Renaming
 
-@[reducible, simp]
 def id {γ} : γ →ʳ γ := fun {{_}} x => x
 
 notation "𝟙ʳ" => Renaming.id
 
-@[simp]
 def comp {γ δ η} (g : δ →ʳ η) (f : γ →ʳ δ) : γ →ʳ η :=
   fun {{_}} x => g (f x)
 
 infixr:90 " ∘ʳ " => Renaming.comp
 
-@[reducible]
 def extend {γ δ} (f : γ →ʳ δ) (η) : γ ⊕ η →ʳ δ ⊕ η
 | _, .varLeft x => .varLeft (f x)
 | _, .varRight y => .varRight y
@@ -31,10 +28,26 @@ def extend_id {γ η} : 𝟙ʳ ⇑ʳ η = @id (γ ⊕ η) := by
 
 def extend_comp {γ δ η θ} {g : δ →ʳ η} {f : γ →ʳ δ}:
   (g ∘ʳ f) ⇑ʳ θ = (g ⇑ʳ θ) ∘ʳ (f ⇑ʳ θ) := by
-  funext α x
+  funext _ x
   cases x <;> rfl
 
+@[reducible]
 def act {γ δ} (f : γ →ʳ δ) : Expr γ → Expr δ
   | x ◃ ts => f x ◃ (fun y => act (f ⇑ʳ _) (ts y))
+
+notation:60 "⟦" f "⟧ʳ " e:61 => Renaming.act f e
+
+theorem act_comp {γ} {e : Expr γ} :
+  ∀ {δ η} {f : γ →ʳ δ} {g : δ →ʳ η}, ⟦ g ∘ʳ f ⟧ʳ e = ⟦ g ⟧ʳ (⟦ f ⟧ʳ e) := by
+  induction e
+  case apply ih =>
+    intros
+    simp [comp, extend_comp]
+    funext
+    apply ih
+
+theorem comp_assoc {γ δ η θ} {f : γ →ʳ δ} {g : δ →ʳ η} {h : η →ʳ θ} :
+  (h ∘ʳ g) ∘ʳ f = h ∘ʳ (g ∘ʳ f) := by rfl
+
 
 end Renaming
--- HigherRankedSyntax/HigherRankedSyntax/Syntax.lean
+++ HigherRankedSyntax/HigherRankedSyntax/Syntax.lean
@@ -31,20 +31,6 @@ inductive Var : Arity → Shape → Type where
 | varLeft : ∀ {α γ δ}, Var α γ → Var α (γ ⊕ δ)
 | varRight : ∀ {α γ δ}, Var α δ → Var α (γ ⊕ δ)
 
-theorem Shape.rank_lt {γ δ} (x : Var γ δ) : γ.rank < δ.rank := by
-  induction x
-  case varHere => apply Nat.lt_add_of_pos_left Nat.zero_lt_one
-  case varLeft η β y ih =>
-    simp [Shape.rank]
-    calc
-      rank γ < 1 + rank γ            := by apply Nat.lt_add_of_pos_left Nat.zero_lt_one
-      _      < 1 + max η.rank β.rank := by apply Nat.add_lt_add_iff_left.2 ; sorry
-  case varRight η β y ih =>
-    simp [Shape.rank]
-    calc
-      rank γ < 1 + rank γ            := by apply Nat.lt_add_of_pos_left Nat.zero_lt_one
-      _      < 1 + max η.rank β.rank := by apply Nat.add_lt_add_iff_left.2 ; sorry
-
 inductive Expr : Shape → Type where
 | apply : ∀ {α γ}, Var α γ → (∀ {β}, Var β α → Expr (γ ⊕ β)) → Expr γ
 
